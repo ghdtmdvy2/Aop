e50eea6f8c6253d617aec4ae10209d22813e5680
hongseungpyo <ghdtmdvy2@gmail.com>
2024-07-21 20:33:25 +0900

    어드바이스 순서 지정

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -16,7 +16,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 // @Import(AspectV1.class)
 // @Import(AspectV2.class)
 // @Import(AspectV3.class)
-@Import(AspectV4Pointcut.class)
+// @Import(AspectV4Pointcut.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
 public class AopTest {
     @Autowired
     OrderService orderService;
